b69eed41e80b35db2f70851dfec868288eb645dc
tinelligino <ginortinelli@gmail.com>
2022-11-13 02:40:25 -0300

    Version8

--- ProyectoFinalTinelliMonjo/mensajeria/models.py
+++ ProyectoFinalTinelliMonjo/mensajeria/models.py
@@ -1,2 +1,20 @@
-from django.db import models
 
+from django.db import models
+from datetime import datetime
+
+
+class Room(models.Model):
+    """Room : Room unico que agrupa mensajes para el CHAT"""
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación",blank=True,null=True)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición",blank=True,null=True)
+
+
+class Message(models.Model):
+    """Message : Message por Usuario agrupados por Room"""
+    value = models.CharField(max_length=255)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=50)
+    room = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación",blank=True,null=True)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición",blank=True,null=True)
---
+++ ProyectoFinalTinelliMonjo/mensajeria/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.urls import path
+from django.conf import settings
+from .views import room, checkview, send, getMessages
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages')
+]
+
+if settings.DEBUG:
+     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
